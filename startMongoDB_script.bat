a554f7485518f105c0659e18ea7872f3ceab29e8
SimoneRighitto <simone.righitto@heig-vd.ch>
2014-10-27 13:47:25 +0100

    added MongoDB start script

---
+++ startMongoDB_script.bat
@@ -0,0 +1,7 @@
+@echo on
+title MongoDB server
+echo starting mongo DB using databaseData directory
+
+"C:\Program Files\MongoDB 2.6 Standard\bin\mongod.exe" --dbpath "C:\Program Files\MongoDB 2.6 Standard\databaseData"
+
+pause
