ff6f7f8130f37b23f58f7e1000c9ffedfc8c8df5
SimoneRighitto <simone.righitto@heig-vd.ch>
2014-11-17 17:25:22 +0100

    prof/student/both role bug fixed !

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -5,37 +5,35 @@ angular.module('twebEasyLearningApp')
     $scope.user = {};
     $scope.errors = {};
 
-    $scope.login = function(form) {
+    $scope.login = function (form) {
       $scope.submitted = true;
 
-      if(form.$valid) {
+      if (form.$valid) {
         Auth.login({
           email: $scope.user.email,
           password: $scope.user.password
         })
-        .then( function() {
-          // Logged in, redirect to home
-            if(Auth.isProfAndUser() === true){
-            $location.path('/');  
+          .then(function () {
+            // Logged in, redirect to home
+            if (Auth.isBoth() === true) {
+              $location.path('/');
+            } else if (Auth.isProf() === true) {
+              $location.path('/profView');
+            } else if (Auth.isStudent()) {
+              $location.path('/studentView');
+            } else {
+              //no role found going to / path;
+              $location.path('/');
             }
-            else if(Auth.isProf()===true){
-            $location.path('/profView');    
-            }
-            else if(Auth.isStudent()===true){
-            $location.path('/studentView');
-            }
-            else{
-             $location.path('/');   
-            }
-          
-        })
-        .catch( function(err) {
-          $scope.errors.other = err.message;
-        });
+
+          })
+          .catch(function (err) {
+            $scope.errors.other = err.message;
+          });
       }
     };
 
-    $scope.loginOauth = function(provider) {
+    $scope.loginOauth = function (provider) {
       $window.location.href = '/auth/' + provider;
     };
   });
