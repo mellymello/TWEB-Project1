843829e628d065740528c441ae33ddffd8d16ca6
calixte.melly <calixte.melly@heig-vd.ch>
2014-11-17 14:54:22 +0100

    added pdf preview for lessonStart

--- client/app/studentStart/studentStart.controller.js
+++ client/app/studentStart/studentStart.controller.js
@@ -1,6 +1,79 @@
 'use strict';
 
-angular.module('twebEasyLearningApp')
-  .controller('StudentstartCtrl', function ($scope, $http) {
-    $scope.message = 'Hello';
+angular.module('twebEasyLearningApp').controller('StudentstartCtrl', function ($scope, $http) {
+
+  $http.get('/api/lectures').success(function (msg) {
+
   });
+
+
+  var pdfUrl = 'data/testFile.pdf';
+
+
+  //
+  // Disable workers to avoid yet another cross-origin issue (workers need
+  // the URL of the script to be loaded, and dynamically loading a cross-origin
+  // script does not work).
+  //
+  // PDFJS.disableWorker = true;
+
+  //
+  // In cases when the pdf.worker.js is located at the different folder than the
+  // pdf.js's one, or the pdf.js is executed via eval(), the workerSrc property
+  // shall be specified.
+  //
+  // PDFJS.workerSrc = 'components/pfjs/pdf.worker.js';
+
+  var pdfDoc = null,
+    pageNum = 1,
+    pageRendering = false,
+    pageNumPending = null,
+    scale = 0.8,
+    canvas = document.getElementById('the-canvas'),
+    ctx = canvas.getContext('2d');
+
+  /**
+   * Get page info from document, resize canvas accordingly, and render page.
+   * @param num Page number.
+   */
+  function renderPage(num) {
+    pageRendering = true;
+    // Using promise to fetch the page
+    pdfDoc.getPage(num).then(function (page) {
+      var viewport = page.getViewport(scale);
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
+
+      // Render PDF page into canvas context
+      var renderContext = {
+        canvasContext: ctx,
+        viewport: viewport
+      };
+      var renderTask = page.render(renderContext);
+
+      // Wait for rendering to finish
+      renderTask.promise.then(function () {
+        pageRendering = false;
+        if (pageNumPending !== null) {
+          // New page rendering is pending
+          renderPage(pageNumPending);
+          pageNumPending = null;
+        }
+      });
+    });
+  }
+
+  /**
+   * Asynchronously downloads PDF.
+   */
+  PDFJS.getDocument(pdfUrl).then(function (pdfDoc_) {
+    pdfDoc = pdfDoc_;
+
+    // Initial/first page rendering
+    renderPage(pageNum);
+  });
+
+
+
+
+});
--- server/routes.js
+++ server/routes.js
@@ -9,7 +9,7 @@ var errors = require('./components/errors');
 module.exports = function(app) {
 
   // Insert routes below
-  app.use('/y', require('./api/lecture'));
+  app.use('/api/lectures', require('./api/lecture'));
   app.use('/api/chats', require('./api/chat'));
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
